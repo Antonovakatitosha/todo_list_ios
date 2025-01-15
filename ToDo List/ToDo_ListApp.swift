4f6c9ef31b4d41462c5e2393ade847633fa1e0fc
Kate Evdochenko <antonovakatitosha@gmail.com>
2025-01-15 18:48:25 +0100

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Kate Evdochenko on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDo ListTests/ToDo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListTests.swift
+//  ToDo ListTests
+//
+//  Created by Kate Evdochenko on 1/15/25.
+//
+
+import Testing
+@testable import ToDo_List
+
+struct ToDo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
